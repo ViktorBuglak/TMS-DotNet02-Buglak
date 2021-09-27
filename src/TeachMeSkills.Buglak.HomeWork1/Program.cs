5290cab923736455962eb93679b14aa8ea17b430
Viktor Buglak <77204696+ViktorBuglak@users.noreply.github.com>
2021-09-27 13:56:10 +0300

    Home work 1 (#4)
    * init: add solution
    
    * fead : add solution of HomeWork1

---
+++ src/TeachMeSkills.Buglak.HomeWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeachMeSkills.Buglak.HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Viktor B.");
+        }
+    }
+}
