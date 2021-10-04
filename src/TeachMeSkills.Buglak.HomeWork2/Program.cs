a651b9a0930abcef6df20164ae43048a1c94b3e6
Viktor Buglak <viktor.buglakk@gmail.com>
2021-10-04 16:33:35 +0300

    feat : add dayHaldler fuction

--- src/TeachMeSkills.Buglak.HomeWork2/Program.cs
+++ src/TeachMeSkills.Buglak.HomeWork2/Program.cs
@@ -2,11 +2,64 @@
 
 namespace TeachMeSkills.Buglak.HomeWork2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Проверка первой датый
+            Console.WriteLine("Введите первую дату");
+            var dateStartAsString = Console.ReadLine();
+            DateTime dateStart; // первая дата
+            bool parsesuccessstart = DateTime.TryParse(dateStartAsString, out dateStart);
+            if (!parsesuccessstart)
+            {
+                Console.WriteLine("Некорректной ввод даты");
+                return;
+            }
+
+            // Проверка второй датый
+            Console.WriteLine("Введите вторую дату");
+            var dateFinishAsString = Console.ReadLine();
+            DateTime dateFinish; // вторая дата
+            bool parsesuccessfinish = DateTime.TryParse(dateFinishAsString, out dateFinish);
+            if (!parsesuccessfinish)
+            {
+                Console.WriteLine("Некорректной ввод даты");
+                return;
+            }
+
+            if (dateStart > dateFinish)
+            {
+                Console.WriteLine("Первая дата должна быть раньше чем вторая");
+                return;
+            }
+
+            //Проверка дня недели
+            Console.WriteLine("Введите день недели(на англ)");
+            var dayAssString = Console.ReadLine();
+            DayOfWeek day;
+            bool isCorrectDay = DayOfWeek.TryParse(dayAssString, out day);
+            if (!isCorrectDay)
+            {
+                Console.WriteLine("Некорректный ввод дня недели");
+                return;
+            }
+            FindAndShowDays(ref dateStart, ref dateFinish, ref day);
+
         }
+
+        public static void FindAndShowDays(ref DateTime dateStart, ref DateTime dateFinish, ref DayOfWeek day)
+        {
+            while (dateStart <= dateFinish)
+            {
+                if (dateStart.DayOfWeek == day)
+                {
+                    Console.WriteLine($"{dateStart} - {dateStart.DayOfWeek}");
+                }
+                dateStart = dateStart.AddDays(1);
+            }
+
+        }
+
     }
 }
