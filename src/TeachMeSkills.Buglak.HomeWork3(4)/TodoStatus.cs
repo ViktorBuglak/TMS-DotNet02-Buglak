1e254e5ebb8a98f7340a39c774e50277496354c9
Viktor Buglak <viktor.buglakk@gmail.com>
2021-10-27 02:13:04 +0300

    feat: add solution homework3(4)

---
+++ src/TeachMeSkills.Buglak.HomeWork3(4)/TodoStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachMeSkills.Buglak.HomeWork3_4_
+{
+    public enum TodoStatus
+    {
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Backlog.
+        /// </summary>
+        Backlog = 1,
+
+        /// <summary>
+        /// In progress.
+        /// </summary>
+        InProgress = 2,
+
+        /// <summary>
+        /// Done.
+        /// </summary>
+        Done = 3,
+
+        /// <summary>
+        /// Canceled.
+        /// Canceled.
+        /// </summary>
+        Canceled = 4,
+    }
+}
